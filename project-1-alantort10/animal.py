f74c69b67be5ebbdc12a0cae2c0b787bc89705d8
Alan Tort <71615968+alantort10@users.noreply.github.com>
2021-06-23 11:52:53 -0700

    Add files via upload
    Project 1

---
+++ project-1-alantort10/animal.py
@@ -0,0 +1,7 @@
+# Author: Alan Tort
+# Date: 6/22/2021
+# Description: Assignment 1
+# Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
